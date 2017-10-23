f93f2078b36b6b0d72e8eeedfe1739e8a6b36232
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2017-10-22 19:10:36 -0500

    Update student admission.py
    Added an alternate way to set up the graph

--- student admission.py
+++ student admission.py
@@ -116,3 +116,61 @@ with tf.Session(graph=graph) as sess:
             print "loss at step %d: %f" % (step,1)
             print "minibatch acc: %.1f%%"  % accuracy(predictions, batch_labels)
     print("Test accuracy: %.1f%%" % accuracy(test_pred.eval(), y_test))
+    
+hl1 = {'weights': tf.Variable(tf.truncated_normal([num_features, h1_nodes])),
+       'biases' : tf.Variable(tf.zeros([h1_nodes]))}
+hl2 = {'weights': tf.Variable(tf.truncated_normal([h1_nodes, h2_nodes])),
+       'biases' : tf.Variable(tf.zeros([h2_nodes]))}
+hl3 = {'weights': tf.Variable(tf.truncated_normal([h2_nodes, h3_nodes])),
+       'biases' : tf.Variable(tf.zeros([h3_nodes]))}
+out = {'weights': tf.Variable(tf.truncated_normal([h3_nodes, num_labels])),
+       'biases' : tf.Variable(tf.zeros([num_labels]))}
+
+keep_prob = tf.placeholder(tf.float32)
+lg1 = tf.add(tf.matmul(X_train, hl1['weights']), hl1['biases'])
+lg1 = tf.nn.relu(lg1)
+lg1 = tf.nn.dropout(lg1, keep_prob)
+
+lg2 = tf.add(tf.matmul(lg1, hl2["weights"]), hl2['biases'])
+lg2 = tf.nn.relu(lg2)
+lg2 = tf.nn.dropout(lg2, keep_prob)
+
+lg3 = tf.add(tf.matmul(lg2, hl3['weights']), hl3['biases'])
+lg3 = tf.nn.relu(lg3)
+lg3 = tf.nn.dropout(lg3, keep_prob)
+
+logits = tf.matmul(lg3, out['weights']) + out['biases']
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=y_train))
+optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+tf_train_features = tf.placeholder(tf.float32, shape=(batch_size, num_features))
+tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+tf_valid_features = tf.constant(X_valid)
+tf_test_features = tf.constant(X_test)
+                      
+train_pred = tf.nn.softmax(logits)
+valid_pred = tf.nn.softmax(tf.matmul(X_valid, out['weights']), out['biases'])
+test_pred = tf.nn.softmax(tf.matmul(X_test, out['weights']), out['biases'])
+
+num_steps = 3001
+with tf.Session() as sess:
+    tf.global_variables_initializer().run()
+
+    for step in range(num_steps):
+        #set up a batch generator and feed dict        
+        offset = (step*batch_size) % (y_train.shape[0] - batch_size)
+        batch_data = X_train[offset:(offset + batch_size), :]
+        batch_labels = y_train[offset:(offset + batch_size), :]
+        feed_dict = {tf_train_features:batch_data,
+                     tf_train_labels:batch_labels}
+        
+        #run session, printing log loss and minibatch accuracy
+        _, log_loss, predictions = sess.run(
+            [optimizer, loss, train_pred], feed_dict=feed_dict)
+        if (step % 100 == 0):
+            print "loss at step %d: %f" % (step,1)
+            print "minibatch acc: %.1f%%"  % accuracy(predictions, batch_labels)
+    print("Test accuracy: %.1f%%" % accuracy(test_pred.eval(), y_test))
+
