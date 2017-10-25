7d27fa3533181709b91441a3a56b0ae0f79a4a84
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2017-10-25 15:56:46 -0500

    Update student_admissions_keras.py
    fit and evaluate model

--- student_admissions_keras.py
+++ student_admissions_keras.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
 import keras, math
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam, Adagrad, rmsprop
+
+###try relu and sigmoid
+###try categorical_crossentropy, mean_squared_error,
+###try rmsprop, adam, ada
 
 #load dataset
 df=pd.read_csv('/Users/dylanrutter/Downloads/aind2-dl-master/student_data.csv')
@@ -28,3 +34,25 @@ y_train, y_valid, y_test = y[:900], y[900:1050], y[1050:1200]
 #set up univeral variables
 num_features = X_train.shape[1]
 num_labels = y_train.shape[1]
+
+# Building the model
+# Note that filling out the empty rank as "0", gave us an extra column,
+# for "Rank 0" students.
+# Thus, our input dimension is 7 instead of 6.
+model = keras.models.Sequential()
+model.add(Dense(128, activation='relu', input_shape=(7,)))
+model.add(Dropout(.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(.1))
+model.add(Dense(2, activation='softmax'))
+
+# Compiling the model
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',\
+              metrics=['accuracy'])
+#model.summary()
+model.fit(X_train, y_train, nb_epoch=200, batch_size=100, verbose=0)
+
+score = model.evaluate(X_train, y_train)
+print("\n Training Accuracy:", score[1])
+score = model.evaluate(X_test, y_test)
+print("\n Testing Accuracy:", score[1])
